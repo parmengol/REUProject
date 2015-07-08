1bffbd2b327bb675330d3186ae753548fd2e60bf
parmengol <parmengol@knights.ucf.edu>
2015-07-08 10:52:59 -0400

    fixed deletion of stray points in localize

--- php/deletereading.php
+++ php/deletereading.php
@@ -4,7 +4,14 @@ $db = new DB_Functions();
 $x = $_POST["x"];
 $y = $_POST["y"];
 
-$res = $db->delete($x,$y);
-echo "hi";
+$res = $db->delete($x-0.01,$x+0.01,$y-0.01,$y+0.01);
+if ($res) {
+    echo $x;
+    echo ",";
+    echo $y;
+}
+else {
+    echo "not hi";
+}
 ?>
 
